5f15cdae2596998d1474277d89f36516d7a4bfa5
yoheiro <45040078+yoheiro@users.noreply.github.com>
2020-12-01 12:03:15 +0900

    add save log

--- www/js/index.js
+++ www/js/index.js
@@ -32,7 +32,12 @@ function onDeviceReady() {
         };
             var setid ="beacon"+pluginResult.beacons[0].minor;
             document.getElementById(setid).innerHTML = "<div><p>ID:"+pluginResult.beacons[0].major+"   status:"+pluginResult.beacons[0].minor+"</p><p>"+" 距離 = "+distancebe+"  推定:"+susdis2+"</p></div>";
-        
+
+            
+        //log
+        if(logging==true){
+            loglog(beaconData);
+        }
         };
     // delegate の設定
     cordova.plugins.locationManager.setDelegate(delegate);
@@ -48,4 +53,81 @@ function onDeviceReady() {
 
 
     }
+///////////logging
+var logging =false;
+var logitem=[];
+    function startlog(){
+        
+        logging =true;
+        console.log(logging);
+    };
+    
+    function loglog(beaconData){
+        let date = new Date();
+        let starttime=date.getFullYear()
+        + '/' + ('0' + (date.getMonth() + 1)).slice(-2)
+        + '/' + ('0' + date.getDate()).slice(-2)
+        + ' ' + ('0' + date.getHours()).slice(-2)
+        + ':' + ('0' + date.getMinutes()).slice(-2)
+        + ':' + ('0' + date.getSeconds()).slice(-2)
+            let logele=[starttime,beaconData.major,beaconData.minor,distancebe]
+         logitem.push(logele);
+         console.log(logitem);
+    };
+        
+    function  stoplog(){
+        logging =false;
+
+
+    };
+   function  handleDownload(){
+       console.log(logitem);
+        //savelog
+        let csvContent = "data:text/csv;charset=utf-8\n" 
+      + logitem.map(e => e.join(",")).join("\n");
+
+      console.log(csvContent);
+
+      //DL csv
+      var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+      var content = csvContent;
+      var blob = new Blob([ bom, content ], { "type" : "text/csv" });
+    
+    //
+    window.requestFileSystem(LocalFileSystem.TEMPORARY, 0,
+        function(fileSystem) {
+    createFile(fileSystem.root, blob, "sample.csv");
+        });
+    function createFile(dirEntry, fileData, fileName) {
+        dirEntry.getFile(fileName, { create: true, exclusive: false }, 
+          function (fileEntry) {
+             // ファイルの保存先
+            console.log(fileEntry.toURL());
+      
+            // ファイルへの書き込み
+            writeFile(fileEntry, fileData);
+          }, function(e) {
+            console.log("error: " + e.code);
+          }
+        );
+      }
+
+      function writeFile(fileEntry, dataObj) {
+        fileEntry.createWriter(function (fileWriter) {
+      
+          fileWriter.onwriteend = function() {
+            alart("保存完了");
+            console.log("Success write()");
+          };
+          fileWriter.onerror = function(e) {
+            console.log("Failed file write: " + e.toString());
+          };
+      
+          fileWriter.write(dataObj);
+        });
+      }
+
+   }
+
+   
     getdis();
